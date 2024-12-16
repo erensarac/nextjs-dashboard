27df8c2a7095fba3d3b470806efa19cbe6982209
erensarac <esarac@outlook.com.tr>
2024-12-16 03:44:58 -0800

    fix: type error on actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,9 +32,10 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
+    // return {
+    //   message: 'Database Error: Failed to Create Invoice.',
+    // };
+    console.log('Database Error: Failed to Create Invoice.')
   }
 
   revalidatePath('/dashboard/invoices');
@@ -57,7 +58,8 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    // return { message: 'Database Error: Failed to Update Invoice.' };
+    console.log('Database Error: Failed to Update Invoice.')
   }
 
   revalidatePath('/dashboard/invoices');
@@ -70,6 +72,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    // return { message: 'Database Error: Failed to Delete Invoice.' };
+    console.log('Database Error: Failed to Delete Invoice.')
   }
 }
